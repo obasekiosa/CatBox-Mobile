4e4de784d77df3b1ccd91d810894f18c504d1661
Osakpolor Obaseki <obasekiosa@gmail.com>
2020-07-10 11:21:15 +0100

    Complete App

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:catbox/ui/cat_list.dart';
+
+void main() async {
+  runApp(new CatBoxApp());
+}
+
+class CatBoxApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.pinkAccent,
+        fontFamily: 'Ubuntu',
+      ),
+      home: new CatList(),
+    );
+  }
+}
